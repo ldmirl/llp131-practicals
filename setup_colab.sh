f22fec986838eb2096118cda2e4cd00257f310c2
Corentin Artaud <corentinartaud@gmail.com>
2022-03-07 10:56:11 +0000

    updated colab setups

--- setup_colab.sh
+++ setup_colab.sh
@@ -2,4 +2,4 @@
 
 apt-get -qq update
 apt-get -qq install -y xvfb
-wget -q https://raw.githubusercontent.com/lulmil/llp131-practical/master/xvfb -O ../xvfb
+wget -q https://raw.githubusercontent.com/lulmil/llp131-practicals/master/xvfb -O ../xvfb
