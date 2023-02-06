6e08bc7a6613fde8533a20004af1faea4704ea6e
Corentin Artaud <corentinartaud@gmail.com>
2023-02-06 11:39:01 +0000

    fixed apt-get update

--- setup_colab.sh
+++ setup_colab.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get -qq update
-apt-get -qq install -y xvfb
+apt-get -q update
+apt-get -q install -y xvfb
 wget -q https://raw.githubusercontent.com/ldmirl/llp131-practicals/master/xvfb -O ../xvfb
